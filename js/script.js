b6279d02a5443fedc39e231a3dbaa338c6f976ea
bintikholifah <bintikholifah1616@gmail.com>
2023-08-17 20:06:47 +0700

    upload project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+// Mendefinisikan fungsi ke calculate berdasarkan button yg di klik.
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    // Jika output "%", replace dengan "/100" sebelum evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "C") {
+    // Jika button "C" di klik, hapus karakter terakhir dari output.
+    output = output.toString().slice(0, -1);
+  } else {
+    // Jika output kosong dan button adalah specialShars maka return...
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+  //   console.log(btnValue);
+};
+
+// Menambahkan event listener ke buttons, panggil calculate() on click.
+buttons.forEach((button) => {
+  //Ketika button di klik memanggil calculate() dengan value sebagai argumen.
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
+
+// console.log(display, buttons);
